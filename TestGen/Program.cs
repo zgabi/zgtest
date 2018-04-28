5341948f9fcf8eb2557718669e5ab419ae4d7294
Zavarkó Gábor <zavarkog@gmail.com>
2018-04-28 13:56:43 +0200

    x

---
+++ TestGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
